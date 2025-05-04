fc1bcc00fe154829466a0600c67b152996fc75d4
Zainab Rangwala <zfr2000@hw.ac.uk>
2025-05-04 17:27:11 +0530

    Added a navigation bar to the Notion Main Page

---
+++ notion-clone/app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,7 @@
+const DocumentPage = () => {
+    return (<div>
+        This is a protected Documents Page
+    </div>);
+}
+
+export default DocumentPage
--- notion-clone/app/(marketing)/_components/heading.tsx
+++ notion-clone/app/(marketing)/_components/heading.tsx
@@ -3,16 +3,41 @@
 import { Button } from "@/components/ui/button";
 import { ArrowRight } from "lucide-react";
 import { Heroes } from "./heroes";
+import { useConvexAuth } from "convex/react";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link";
+import { SignInButton } from "@clerk/nextjs";
 
 export const Heading = () => {
-    return (
-        <div className="max-w-3xl space-y-4">
-            <h1 className="text-3xl sm:text-5xl md:text-6sxl font-bold">Your Ideas,Documents,& Plans. Unified. Welcome to <span className="underline">Notion</span></h1>
-            <h3 className="text-base sm:text-xl md:text-2xl font-medium">Notion is the connected workspace where <br /> better, faster work happens. </h3>
-        <Button>Enter Notion
-        <ArrowRight className="h-4 w-4 ml-2" />
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6sxl font-bold">
+        Your Ideas,Documents,& Plans. Unified. Welcome to{" "}
+        <span className="underline">Notion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Notion is the connected workspace where <br /> better, faster work
+        happens.{" "}
+      </h3>
+      {isLoading && <div className="w-full flex items-center justify-center"><Spinner size="lg"/></div> }
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+            <Link href="/documents">
+          Enter Notion
+          <ArrowRight className="h-4 w-4 ml-2" />
+          </Link>
         </Button>
-        <Heroes />
-        </div>
-    )
-}
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+            <Button>
+                Get Notion Free
+                <ArrowRight className="h-4 w-4 ml-2"/>
+            </Button>
+        </SignInButton>
+      )}
+      <Heroes />
+    </div>
+  );
+};
