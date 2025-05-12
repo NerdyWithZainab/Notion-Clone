3673ee2d72b3248ff871e47ac0420bc2a391afe1
Zainab Rangwala <zfr2000@hw.ac.uk>
2025-05-12 18:29:00 +0530

    I fixed the error of a feature in the search feature and the settings feature is finished

--- notion-clone/app/layout.tsx
+++ notion-clone/app/layout.tsx
@@ -4,6 +4,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
             storageKey="notion-theme"
           >
             <Toaster position="bottom-center" />
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </ConvexClientProvider>
--- notion-clone/convex/documents.ts
+++ notion-clone/convex/documents.ts
@@ -188,16 +188,15 @@ export const getSearch = query({
     const identity = await ctx.auth.getUserIdentity();
     if (!identity) {
       throw new Error("Not authenticated");
-
-      const userId = identity?.subject;
-      const documents = await ctx.db
-        .query("documents")
-        .withIndex("by_user", (q) => q.eq("userId", userId!))
-        .filter((q) => q.eq(q.field("isArchived"), false))
-        .order("desc")
-        .collect();
-
-      return documents;
     }
+    const userId = identity?.subject;
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId!))
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+
+    return documents;
   },
 });
